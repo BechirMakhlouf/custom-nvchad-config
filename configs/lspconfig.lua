ffd61bca0de1a33000b2eef1aec0f64c0701a0c4
bechirMakhlouf <bechirM@proton.me>
2023-08-12 22:51:52 +0100

    my nvchad git config

---
+++ configs/lspconfig.lua
@@ -0,0 +1,30 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "intelephense",
+  "tailwindcss",
+  "bashls",
+  "emmet_ls",
+  "marksman",
+  "gopls",
+  "pyright",
+  'awk_ls',
+  'jdtls',
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- lspconfig.pyright.setup { blabla}
+--
---
+++ init.lua
@@ -0,0 +1,11 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+-- require("leap").add_default_mappings()
+vim.opt.relativenumber = true
+
+-- require('~/.local/share/nvim/lazy/leap.nvim/plugin/init.lua').add_default_mappings()
